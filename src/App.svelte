2548aa6c9a9f3ad4b42224e94ddbd8d12176d106
Rodrigo Addor <me@rodrick.dev>
2022-05-12 17:44:34 +0200

    feat: basic grid logic

--- src/App.svelte
+++ src/App.svelte
@@ -2,19 +2,103 @@
   import type { TopAppBarComponentDev } from '@smui/top-app-bar';
   import TopAppBar, { Row, Section, Title, AutoAdjust } from '@smui/top-app-bar';
 
-  // import GameGrid from './components/GameGrid.svelte';
+  import TextField from '@smui/textfield';
+
   import GridLines from './components/GridLines.svelte';
 
   let topAppBar: TopAppBarComponentDev;
+
+  let size: [number, number] = [3, 3];
+  let error: boolean = false;
+
+  let sizeInput: string = size.join('x');
+  let oldSizeInput = sizeInput;
+
+  const isValid = (amount: any): boolean => {
+    const num = parseInt(amount);
+    if (isNaN(num)) return false;
+    return amount > 0 && amount < 20;
+  };
+
+  const validateSize = (e: CustomEvent<any>) => {
+    const { inputType, data: char } = e as any as InputEvent;
+    const target = e.target as HTMLInputElement;
+
+    if (/^\d*x\d*$/.test(target.value)) {
+      const [x, y] = target.value.split('x').map(Number);
+      error = !(isValid(x) && isValid(y));
+      if (!error) {
+        size = target.value.split('x').map(Number) as [number, number];
+      }
+      oldSizeInput = target.value;
+    } else {
+      setTimeout(() => target.setSelectionRange(target.selectionStart - 1, target.selectionStart - 1));
+      sizeInput = '';
+      sizeInput = oldSizeInput;
+
+      const select = (pos: number) =>
+        setTimeout(() => {
+          target.setSelectionRange(target.selectionStart + pos, target.selectionStart + pos);
+          console.log(target.selectionStart + pos);
+        });
+
+      console.log(char, isNaN(parseInt(char)));
+
+      if (inputType === 'deleteContentBackward') {
+        // select(-1);
+      } else if (char !== null && isNaN(parseInt(char))) {
+        select(1);
+      }
+    }
+  };
+
+  let squares: boolean[] = [];
+  let lines: boolean[] = [];
 </script>
 
-<template lang="pug">
-  TopAppBar(bind:this='{topAppBar}' variant='fixed' dense)
-    Row
-      Section
-        Title Gridd
-      //- Section(align='end' toolbar)
-        //- iconbutton.material-icons(aria-label='Bookmark this page') bookmark
-  AutoAdjust(topAppBar='{topAppBar}')
-    GridLines(size='{[5,5]}')
+<template>
+  <TopAppBar bind:this={topAppBar} variant="fixed" dense>
+    <Row>
+      <Section>
+        <Title>Gridd</Title>
+      </Section>
+      <Section align="end" toolbar>
+        <TextField
+          class="size-field"
+          value={sizeInput}
+          on:input={validateSize}
+          variant="outlined"
+          prefix="Size"
+          bind:error
+          dense
+        />
+        <!-- iconbutton.material-icons(aria-label='Bookmark this page') bookmark -->
+      </Section>
+    </Row>
+  </TopAppBar>
+  <AutoAdjust {topAppBar}>
+    <GridLines bind:size bind:squares bind:lines />
+  </AutoAdjust>
 </template>
+
+<style lang="sass">
+  @use '@material/theme/color-palette' as palette
+
+  :global(.size-field)
+    background-color: transparentize(#2a2a2e, 0.4)
+    border: none
+    border-radius: 4px
+    height: 32px
+    margin-right: 4px
+    width: 12em
+    transition: all 0.3s
+
+  :global(.size-field[error='true'])
+    background-color: palette.$red-900
+
+  :global(.size-field .mdc-text-field__input)
+    margin-left: 12px
+      
+  :global([class^='mdc-notched-outline'])
+    border: none
+</style>
--- src/components/GridLines.svelte
+++ src/components/GridLines.svelte
@@ -1,142 +1,251 @@
+<script lang="ts" context="module">
+  export type LineCallback = (lineIndex: number) => void;
+  export type SquareData = boolean;
+
+  enum ElementType {
+    DOT = 'dot',
+    HORIZONTAL_LINE = 'horizontal-line',
+    VERTICAL_LINE = 'vertical-line',
+    SQUARE = 'square',
+  }
+</script>
+
 <script lang="ts">
   export let size: [number, number];
 
-  const hLines: boolean[][] = Array.from({ length: size[1] + 1 }, () => Array.from({ length: size[0] }, () => false));
-  const vLines: boolean[][] = Array.from({ length: size[0] + 1 }, () => Array.from({ length: size[1] }, () => false));
+  export let lines: boolean[];
+  export let squares: boolean[];
 
-  const hDots: boolean[][] = Array.from({ length: size[1] + 1 }, () =>
-    Array.from({ length: size[0] + 1 }, () => false)
-  );
+  let elements: { type: ElementType; active: boolean; handler?: () => void }[];
 
-  const vDots: boolean[][] = Array.from({ length: size[0] + 1 }, () =>
-    Array.from({ length: size[1] + 1 }, () => false)
-  );
-
-  const toggleLine = (x, line) => {
-    return () => console.log({ x, line });
+  const getElementType = (col: number, line: number): ElementType => {
+    if (col % 2 == 0 && line % 2 == 0) return ElementType.DOT;
+    else if (col % 2 == 0 && line % 2) return ElementType.VERTICAL_LINE;
+    else if (col % 2 && line % 2 == 0) return ElementType.HORIZONTAL_LINE;
+    else if (col % 2 && line % 2) return ElementType.SQUARE;
+    else throw Error('Invalid state: invalid column and line combination');
   };
+
+  const findGroup = (n: number, ...groups: number[]) => {
+    let cur = 0;
+    let group = 0;
+    let groupsAmount: number[] = Array.from({ length: groups.length }, () => 0);
+
+    do {
+      const curGroup = group % groups.length;
+
+      if (n >= cur && n < cur + groups[curGroup]) {
+        return {
+          index: n - cur, // element's index in the group instance the element is in
+          group: groups[curGroup], // group the element is in
+          groupNumber: groupsAmount[curGroup], // index of the group instance the element is in
+          groupIndex: groupsAmount[curGroup] * groups[curGroup] + (n - cur), // element's index in the group the element is in, cummulative
+        };
+      }
+
+      cur += groups[curGroup];
+      group++;
+      groupsAmount[curGroup]++;
+      if (group >= groups.length) group = 0;
+    } while (cur <= n);
+  };
+
+  const checkCompletion = (lineIndex: number) => {
+    const { index, group, groupIndex } = findGroup(lineIndex, size[0], size[0] + 1);
+    const lineType = group === size[0] ? ElementType.HORIZONTAL_LINE : ElementType.VERTICAL_LINE;
+  };
+
+  const getRelatedSquares = (lineIndex: number) => {
+    const result: { square: number; lines: number[] }[] = [];
+    const getLines = (...offsets: number[]) => offsets.map((offset) => lineIndex + offset);
+
+    const xOffset = size[0];
+
+    const { index, group, groupIndex, groupNumber } = findGroup(lineIndex, xOffset, xOffset + 1);
+    const lineType = group === xOffset ? ElementType.HORIZONTAL_LINE : ElementType.VERTICAL_LINE;
+
+    if (lineType === ElementType.HORIZONTAL_LINE) {
+      if (groupNumber === 0) {
+        // top row, only one square (below)
+        result.push({ square: groupIndex, lines: getLines(0, xOffset, xOffset + 1, xOffset * 2 + 1) });
+      } else if (groupNumber === size[1]) {
+        // bottom row, only one square (above)
+        result.push({ square: groupIndex - xOffset, lines: getLines(0, -xOffset, -xOffset - 1, -xOffset * 2 - 1) });
+      } else {
+        // middle rows, two squares (above and below)
+        result.push({ square: groupIndex - xOffset, lines: getLines(0, -xOffset, -xOffset - 1, -xOffset * 2 - 1) });
+        result.push({ square: groupIndex, lines: getLines(0, xOffset, xOffset + 1, xOffset * 2 + 1) });
+      }
+    } else {
+      if (index === 0) {
+        // start column, only one square (right)
+        result.push({ square: groupNumber * xOffset, lines: getLines(0, -xOffset, 1, xOffset + 1) });
+      } else if (index === xOffset) {
+        // end colunm, only one square (left)
+        result.push({ square: (groupNumber + 1) * xOffset - 1, lines: getLines(0, -xOffset - 1, -1, xOffset) });
+      } else {
+        // middle columns, two squares (left and right)
+        result.push({ square: groupNumber * xOffset + index, lines: getLines(0, -xOffset, 1, xOffset + 1) });
+        result.push({ square: groupNumber * xOffset + index - 1, lines: getLines(0, -xOffset - 1, -1, xOffset) });
+      }
+    }
+
+    return result;
+  };
+
+  const getRelatedLines = (lineIndex: number): number[] => {
+    const result: number[] = [];
+
+    const addResults = (...offsets: number[]) => offsets.forEach((offset) => result.push(lineIndex + offset));
+
+    const { index, group } = findGroup(lineIndex, size[0], size[0] + 1);
+    const lineType = group === size[0] ? ElementType.HORIZONTAL_LINE : ElementType.VERTICAL_LINE;
+
+    if (lineType === ElementType.HORIZONTAL_LINE) {
+      addResults(-size[0] * 2 - 1, size[0] * 2 + 1, -size[0], -size[0] - 1, size[0], size[0] + 1);
+      // addResults(-7, -4, -size[0], size[0], 4, 7);
+    } else if (index === 0) {
+      addResults(-size[0], 1, size[0] + 1);
+    } else if (index === size[0]) {
+      addResults(-size[0] - 1, -1, size[0]);
+    } else {
+      addResults(-size[0] - 1, -size[0], -1, 1, size[0], size[0] + 1);
+    }
+
+    return result.filter((index) => index >= 0 && index < lines.length);
+  };
+
+  $: {
+    let lineIndex = 0;
+    let squareIndex = 0;
+
+    elements = Array.from({ length: (size[0] * 2 + 1) * (size[1] * 2 + 1) }, (_, i) => {
+      const col = i % (size[0] * 2 + 1);
+      const line = Math.floor(i / (size[0] * 2 + 1));
+
+      const type = getElementType(col, line);
+      let active = false;
+      let handler: (() => void) | undefined;
+
+      if (type === ElementType.VERTICAL_LINE || type === ElementType.HORIZONTAL_LINE) {
+        let thisLineIndex = lineIndex++;
+        active = lines[thisLineIndex];
+        lines[thisLineIndex] ??= false;
+        handler = () => {
+          // set clicked line to active
+          lines[thisLineIndex] = true;
+
+          // verify if a square can be activated
+          getRelatedSquares(thisLineIndex).forEach(({ square, lines: relatedLines }) => {
+            if (relatedLines.every((line) => lines[line])) {
+              squares[square] = true;
+            }
+          });
+
+          // getRelatedSquares(thisLineIndex).forEach(({ square, lines: relatedLines }) => {
+          //   squares[square] = true;
+          //   relatedLines.forEach((line) => (lines[line] = true));
+          // });
+
+          // trigger updates if needed
+          squares = squares;
+          lines = lines;
+        };
+      } else if (type === ElementType.SQUARE) {
+        let thisSquareIndex = squareIndex++;
+        active = squares[thisSquareIndex];
+        squares[thisSquareIndex] ??= false;
+      }
+
+      return { type, active, handler };
+    });
+  }
+
+  /*-- Reset grid on size change --*/
+
+  let oldSize = size;
+
+  $: {
+    if (size.join('x') !== oldSize.join('x')) {
+      lines = Array.from({ length: lines.length }, () => false);
+      squares = Array.from({ length: squares.length }, () => false);
+      oldSize = size;
+    }
+  }
 </script>
 
-<template lang="pug">
-  .outer-container
-    .inner-container
-      .hWrapper
-        +each('hLines as x')
-          .hLines
-            +each('x as line')
-              .hLine(on:click='{toggleLine(x, line)}')
-
-      .vWrapper
-        +each('vLines as y')
-          .vLines
-            +each('y as line')
-              .vLine
-      
-      .hDotsWrapper
-        +each('hDots as x')
-          .hDotsLines
-            +each('x as dot')
-              .hDot
-                .dot +
+<template>
+  <div class="wrapper" style:--rows={size[1]} style:--columns={size[0]}>
+    {#each elements as item, index}
+      {#if item.type === ElementType.DOT}
+        <div class="dot" />
+      {:else if item.type === ElementType.HORIZONTAL_LINE}
+        <div class="line horizontal hoverable" class:active={item.active} on:click={item.handler} />
+      {:else if item.type === ElementType.VERTICAL_LINE}
+        <div class="line vertical hoverable" class:active={item.active} on:click={item.handler} />
+      {:else}
+        <div class="square" class:active={item.active} />
+      {/if}
+    {/each}
+  </div>
 </template>
 
 <style lang="sass">
-  :global(#app)
-    height: 100vh
-    width: 100vw
+  @use '@material/theme/color-palette' as palette
 
-  .outer-container
-    height: 100%
-    width: 100%
-    padding: 2rem
-  
-  .inner-container
-    height: 100%
-    width: 100%
-    position: relative
+  :root
+    --thickness: 4px
+    --grid-color: white
 
-  .hWrapper
-    position: absolute
-    top: 0
-    left: 0
-    bottom: 0
-    right: 0
-    display: flex
-    flex-direction: column
-    justify-content: space-between
-    aspect-ratio: 1/1
-    pointer-events: none
-  
-    .hLines
-      display: flex
-      flex-direction: row
-
-      .hLine
-        flex: 1
-        height: 1px
-        background-color: white
-        margin: 0 10px
-        cursor: pointer
-        pointer-events: auto
-        opacity: 0
-
-        &:hover
-          opacity :1
-  
-  .vWrapper
-    position: absolute
-    top: 0
-    left: 0
-    bottom: 0
-    right: 0
-    display: flex
-    flex-direction: row
-    justify-content: space-between
-    aspect-ratio: 1/1
-    pointer-events: none
-  
-    .vLines
-      display: flex
-      flex-direction: column
-
-      .vLine
-        flex: 1
-        width: 1px
-        background-color: white
-        margin: 10px 0
-        pointer-events: auto
-        opacity: 0
-
-        &:hover
-          opacity: 1
+  .wrapper
+    display: grid
+    grid-template-columns: repeat(var(--columns), 1rem 4rem) 1rem
+    grid-template-rows: repeat(var(--rows), 1rem 4rem) 1rem
+    margin: 2rem
+    place-items: center
 
   .dot
-    content: '+'
-    transform: translateY(-1.5px)
-  
-  .hDotsWrapper
-    position: absolute
-    top: 0
-    left: 0
-    bottom: 0
-    right: 0
-    display: flex
-    flex-direction: column
-    justify-content: space-between
+    height: var(--thickness)
+    width: var(--thickness)
+    background-color: var(--grid-color)
+    border-radius: 50%
+
+  .line
+    &::after
+      display: block
+      border-radius: var(--thickness)
+      background-color: var(--grid-color)
+      content: ' '
+
+    &.horizontal, &.vertical
+      display: grid
+      place-items: center
+      height: 100%
+      width: 100%
+
+    &.horizontal::after
+      height: var(--thickness)
+      width: 100%
+    
+    &.vertical::after
+      width: var(--thickness)
+      height: 100%
+
+    &.hoverable
+      opacity: 0
+      cursor: pointer
+
+    &.hoverable:hover, &.active
+      opacity: 1
+
+    &.active
+      cursor: auto
+      pointer-events: none
+    
+  .square
+    min-width: 4rem
     aspect-ratio: 1/1
-    pointer-events: none
 
-    .hDotsLines
-      display: flex
-      flex-direction: row
-      justify-content: space-between
-
-      .hDot
-        display: flex
-        justify-content: center
-        align-items: center
-        height: 1px
-        width: 1px
-        background-color: white
-        pointer-events: auto
+    &.active
+      background-color: transparentize(palette.$light-blue-500, 0.5)
 </style>
